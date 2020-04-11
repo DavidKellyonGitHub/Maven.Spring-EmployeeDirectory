3054e9fc199a28cab03ee2e55e3f850262a12008
Dave Kelly <kelly.david.t@gmail.com>
2020-04-11 19:08:45 -0400

    initial commit

---
+++ src/main/java/io/zipcoder/persistenceapp/Entities/EmployeeEntity.java
@@ -0,0 +1,108 @@
+package io.zipcoder.persistenceapp.Entities;
+
+
+import org.apache.catalina.Manager;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class EmployeeEntity {
+    @GeneratedValue
+    @Id
+    Long employeeID;
+    String firstName;
+    String lastName;
+    String title;
+    String phoneNumber;
+    String email;
+    Date hireDate;
+    String manager;
+    Long departmentNumber;
+
+    public EmployeeEntity(Long employeeID, String firstName, String lastName, String title, String phoneNumber, String email, Date hireDate, String manager, Long departmentNumber) {
+        this.employeeID = employeeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.manager = manager;
+        this.departmentNumber = departmentNumber;
+    }
+
+    public Long getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(Long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public Long getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(Long departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+}
